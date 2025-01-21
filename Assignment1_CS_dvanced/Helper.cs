2859ab2d46cbd340d57fe91d26cab0d2649b83ea
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-21 19:28:56 +0200

    Q1

---
+++ Assignment1_CS_dvanced/Helper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1_CS_dvanced
+{
+   
+        internal class Helper<T> where T : IComparable<T>
+        {
+
+            public static void Swap(ref T X, ref T Y)
+            {
+                T temp = X;
+                X = Y;
+                Y = temp;
+            }
+
+
+            public static void BubbleSort(T[] arr)
+            {
+                bool Swapped = false;
+                if (arr is not null)
+                {
+                    for (int i = 0; i < arr.Length; i++)
+                    {
+                        for (int j = 0; j < arr.Length - i - 1; j++)
+                        {
+                            if (arr[j].CompareTo(arr[j + 1]) == 1)
+                                Swap(ref arr[j], ref arr[j + 1]);
+                            Swapped = true;
+                        }
+
+                        if (!Swapped)
+                            break;
+                    }
+                }
+            }
+        }
+    }
+
--- Assignment1_CS_dvanced/Program.cs
+++ Assignment1_CS_dvanced/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+            //The Bubble Sort algorithm has a time complexity of O(n^2) in its worst and average cases, which makes it inefficient for large datasets. How we can optimize the Bubble Sort algorithm 
+            //And implement the code of this optimized bubble sort algorithm
+            int[] arr = { 3, 6, 2, 7, 5, 8 };
+            Helper<int>.BubbleSort(arr);
+
+            foreach (int i in arr)
+            {
+                Console.WriteLine(i);
+            }
+            #endregion
         }
     }
 }
