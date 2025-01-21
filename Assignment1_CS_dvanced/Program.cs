3e9771fef2c52a1e3e70a50f062cd607dd09dac7
AmiraAdel28 <amira.adel282001@gmail.com>
2025-01-21 19:26:04 +0200

    Add project files.

---
+++ Assignment1_CS_dvanced/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment1_CS_dvanced
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
